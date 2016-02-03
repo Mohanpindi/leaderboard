1aea6f7f6557bc4c1cfec51dce11b317e3d6865a
lalit <ljjoshi200@gmail.com>
2016-02-03 19:40:50 +0530

    leaderboard sorting

---
+++ leaderboard.js
@@ -0,0 +1,35 @@
+PlayersList = new Mongo.Collection('Player');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+  'Player': function(){
+  return PlayersList.find({}, {sort : {score: -1}});
+},
+  'selectedClass': function(){
+    var playerid= this._id;
+    var selectedPlayer= Session.get('selectedPlayer');
+    if(playerid==selectedPlayer){
+      return "selected"
+    }
+}
+
+  });
+ Template.leaderboard.events({
+    'click .Player' : function(){
+      console.log("player click");
+      var playerid= this._id;
+      Session.set('selectedPlayer',playerid);
+      var selectedplayer = Session.get('selectedPlayer');
+    },
+    'click .increament': function(){
+      var selectedPlayer= Session.get('selectedPlayer');
+      console.log("debug " + selectedPlayer);
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .decreament': function(){
+      var selectedPlayer= Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+    }
+  });
+}
